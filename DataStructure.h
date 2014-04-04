8a3cd1ecb6b67728169fbfa2eab7da712dcc9212
zhiminx1 <zhiminx1@localhost.localdomain>
2014-04-04 16:07:35 -0700

    Chapter 1

---
+++ DataStructure.h
@@ -0,0 +1,14 @@
+#ifndef _data_structure
+#define _data_structure
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <list>
+#include <deque>
+#include <string>
+using namespace std;
+
+
+#endif
